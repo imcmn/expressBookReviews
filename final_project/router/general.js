ff0ed7314194030a4dfdafd7e53757c505e113e1
imcmn <tunglt167@gmail.com>
2026-03-23 13:22:25 +0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,47 @@
-const express = require('express');
-let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
-const public_users = express.Router();
-
-
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// 1. Get all books (Task 10)
+public_users.get('/', async function (req, res) {
+  try {
+    const getBooks = new Promise((resolve, reject) => {
+      resolve(books);
+    });
+    const bookList = await getBooks;
+    res.send(JSON.stringify(bookList, null, 4));
+  } catch (error) {
+    res.status(500).json({message: "Error fetching books"});
+  }
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// 2. Get book by ISBN (Task 11)
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  new Promise((resolve, reject) => {
+    if (books[isbn]) resolve(books[isbn]);
+    else reject("Book not found");
+  })
+  .then(book => res.send(JSON.stringify(book, null, 4)))
+  .catch(err => res.status(404).json({message: err}));
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// 3. Get book by Author (Task 12)
+public_users.get('/author/:author', async function (req, res) {
+  const author = req.params.author;
+  try {
+    const booksByAuthor = await new Promise((resolve) => {
+      let filtered = Object.values(books).filter(b => b.author === author);
+      resolve(filtered);
+    });
+    res.send(JSON.stringify(booksByAuthor, null, 4));
+  } catch (error) {
+    res.status(404).json({message: "Author not found"});
+  }
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// 4. Get book by Title (Task 13)
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  new Promise((resolve) => {
+    let filtered = Object.values(books).filter(b => b.title === title);
+    resolve(filtered);
+  })
+  .then(result => res.send(JSON.stringify(result, null, 4)));
 });
-
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
-
-module.exports.general = public_users;
